cc35cbf0053b25be6195672e57b347f37750eb5e
Fishrock123 <fishrock123@rocketmail.com>
2014-05-15 13:11:22 -0400

    refactor: standardize is now sane
    Now checks the existing labels against the wanted ones.
    This means less extraneous requests and the ability to
    support removal and renaming via standardize in the future.

--- index.js
+++ index.js
@@ -73,7 +73,11 @@ function* do_all(org, method, opts, done) {
   var repos   = yield* get_repos(org)
   var results = yield* send_labels(org, repos, method, opts)
 
-  log_results(results, opts.name)
+  var i = results.length
+  while (i--) {
+     log_result(results[i], opts.name)
+  }
+
   console.log(done)
 
   return yield results
@@ -83,62 +87,128 @@ function* standardize(args, program) {
   var org         = args[0]
   var config_repo = args[1]
 
+  var repos = yield* get_repos(org)
+
   var res = yield request({
-        url:     'https://api.github.com/repos/' + org + '/' + config_repo
-               + '/contents/config/github_labels.json'
+        url:     'https://api.github.com/repos/' + org + '/' + config_repo + '/contents/config/github_labels.json'
       , headers: header
       , auth:    auth
       , json:    true
     })
   if (res.statusCode !== 200) throw new Error('error retrieving config from repo: ' + JSON.stringify(res.headers) +'\n')
 
-  console.log('\nGitHub rate limit remaining: ' + res.headers['x-ratelimit-remaining'])
+  console.log('GitHub rate limit remaining: ' + res.headers['x-ratelimit-remaining'])
 
-  var config = JSON.parse(new Buffer(res.body.content, 'base64').toString('utf8'))
-  var results = yield* process_config(org, config)
+  var config  = JSON.parse(new Buffer(res.body.content, 'base64').toString('utf8'))
+  var results = yield* _parrallel_standardize(org, repos, config)
+
+  var total = log_results(results)
+
+  console.log('%d label updates across %d repos', total, repos.length)
 
   console.log('done standardizing labels')
 }
 
-function* process_config(org, config) {
+function* _parrallel_standardize(org, repos, config) {
   if (!Array.isArray(config))
     throw new Error('error: github_labels.json must be a json array')
 
-  console.log('standardizing %d labels', config.length)
+  console.log('checking %d labels across %d repos', config.length, repos.length)
 
-  var i = config.length
+  var results = []
+
+  var i = repos.length
   while (i--) {
-    var label = config[i]
-
-    var results = yield* add([org, label.name, label.color])
-
-    var j = results.length
-    while(j--) {
-      var res = results[j]
-
-      if (res.statusCode === 422) {
-
-        var url = 'https://api.github.com' + res.req.path + '/' + label.name
-        var result = yield request({
-            url:     url
-          , headers: header
-          , method:  'PATCH'
-          , json:    { name: label.name, color: label.color }
-          , auth:    auth
-        })
-
-        if (result.statusCode === 200)
-          console.log('label "' + label.name + '" successfully updated at ' + result.request.path)
-        else {
-          console.log(result.request.path)
-          console.log('status: ' + result.statusCode)
-          if (result.body) console.log(result.body)
-        }
-
-        results[j] = result
-      }
-    }
+    results.push(async_repo(org, repos[i], config))
   }
+
+  return yield results
+}
+
+function* async_repo(org, repo, config) {
+
+  var url = 'https://api.github.com/repos/' + org + '/' + repo + '/labels'
+  var res = yield request({
+      url:     url
+    , headers: header
+    , method:  'GET'
+    , json:    true
+    , auth:    auth
+  })
+  if (res.statusCode !== 200) throw new Error('error getting labels from a repo: ' + JSON.stringify(res.headers) +'\n')
+
+  var list = compare_labels(config, res.body)
+
+  var results = []
+
+  var i = list.length
+  while (i--) {
+    item = list[i]
+
+    results.push(request({
+        url:     url + (item.method === 'POST' ? '' : '/' + item.name)
+      , headers: header
+      , method:  item.method
+      , json:    item
+      , auth:    auth
+    }))
+  }
+
+  return yield results
+}
+
+/*
+  var url = 'https://api.github.com' + res.req.path + '/' + label.name
+  var result = yield request({
+      url:     url
+    , headers: header
+    , method:  'PATCH'
+    , json:    { name: label.name, color: label.color }
+    , auth:    auth
+  })
+
+  if (result.statusCode === 200)
+    console.log('label "' + label.name + '" successfully updated at ' + result.request.path)
+  else {
+    console.log(result.request.path)
+    console.log('status: ' + result.statusCode)
+    if (result.body) console.log(result.body)
+  }
+
+  results[j] = result
+}
+*/
+
+function compare_labels(config, existing) {
+  var out = []
+  var i   = config.length
+
+  while (i--) {
+    var wanted = config[i]
+    var next   = false
+    var j      = existing.length
+    var current
+
+    while (j--) {
+      current = existing[j]
+      if (wanted.name !== current.name) continue
+
+      next = {
+          name:   wanted.name
+        , color:  wanted.color
+        , method: 'PATCH'
+      }
+      break
+    }
+    if (next && wanted.color === current.color) continue
+
+    out.push(next || {
+        name:   wanted.name
+      , color:  wanted.color
+      , method: 'POST'
+    })
+  }
+  return out
 }
 
 function* get_repos(org) {
@@ -179,27 +249,41 @@ function* send_labels(org, repos, method, opts) {
   return yield arr
 }
 
-function log_results(results, label) {
-  var i = results.length
+function log_results(results) {
+  var total = 0
+  var i     = results.length
+
   while (i--) {
-    var result = results[i]
+    var sub = results[i]
+    var j   = sub.length
 
-    if (result.statusCode === 422)
-      console.log('label "' + label + '" already exists at ' + result.request.path)
-
-    else if (result.statusCode === 200)
-      console.log('label "' + label + '" successfully updated at ' + result.request.path)
-
-    else if (result.statusCode === 201)
-      console.log('label "' + label + '" successfully created at ' + result.request.path)
-
-    else if (result.statusCode === 204)
-      console.log('label "' + label + '" successfully deleted from ' + result.request.path)
-
-    else {
-      console.log(result.request.path)
-      console.log('status: ' + result.statusCode)
-      if (result.body) console.log(result.body)
+    while (j--) {
+      total++
+      log_result(sub[j])
     }
+  }
+
+  return total
+}
+
+function log_result(result, label) {
+  label = label || result.body.name
+
+  if (result.statusCode === 422)
+    console.log('label "' + label + '" already exists at ' + result.request.path)
+
+  else if (result.statusCode === 200)
+    console.log('label "' + label + '" successfully updated at ' + result.request.path)
+
+  else if (result.statusCode === 201)
+    console.log('label "' + label + '" successfully created at ' + result.request.path)
+
+  else if (result.statusCode === 204)
+    console.log('label "' + label + '" successfully deleted from ' + result.request.path)
+
+  else {
+    if (result.request.path) console.log(result.request.path)
+    console.log('status: ' + result.statusCode)
+    if (result.body) console.log(result.body)
   }
 }
