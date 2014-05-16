a045510db38baa30a7261105eb7a657dea08b8e8
Fishrock123 <fishrock123@rocketmail.com>
2014-05-16 11:20:09 -0400

    add comments to index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var co      = require('co')
 var request = require('co-request')
 
+/*
+ * checks that a string is a valid hex color code without the preceding `#`
+ */
 var valid_color = /^([0-9A-F]{3}$|[0-9A-F]{6}$)/i
 
+/*
+ * GitHub api requires a `User-Agent` header.
+ */
 var header = { 'User-Agent': 'org-labels' }
 
+/*
+ * GitHub auth variables.
+ * Set either user+pass or just the token (recommended) in your environment
+ *  before running this tool.
+ *
+ * The token is just a user access token, you can generate a new token
+ *  in your GitHub Account Settings, under the Security tab.
+ */
 var GITHUB_USERNAME  = process.env.GITHUB_USERNAME
 var GITHUB_PASSWORD  = process.env.GITHUB_PASSWORD
 var GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN
@@ -25,12 +39,16 @@ if (GITHUB_API_TOKEN) {
   throw new Error('requires a personal env.GITHUB_API_TOKEN or both env.GITHUB_USERNAME and env.GITHUB_PASSWORD')
 }
 
+
 module.exports.add    = add
 module.exports.remove = remove
 module.exports.update = update
 module.exports.rename = rename
 module.exports.standardize = standardize
 
+/*
+ * Adds a label with the specified name and color to all repos in an org.
+ */
 function* add(args, program) {
   var org   = args[0]
   var label = args[1]
@@ -42,6 +60,9 @@ function* add(args, program) {
   return yield* handle_label(org, 'POST', { name: label, color: color }, 'done adding labels')
 }
 
+/*
+ * Removes a label with the specified name from all repos in an org.
+ */
 function* remove(args, program) {
   var org   = args[0]
   var label = args[1]
@@ -49,6 +70,9 @@ function* remove(args, program) {
   return yield* handle_label(org, 'DELETE', { name: label, ext: label }, 'done removing labels')
 }
 
+/*
+ * Updates an existing label with the specified name to the specified color for all repos in an org.
+ */
 function* update(args, program) {
   var org   = args[0]
   var label = args[1]
@@ -60,6 +84,9 @@ function* update(args, program) {
   return yield* handle_label(org, 'PATCH', { name: label, color: color, ext: label }, 'done updating labels')
 }
 
+/*
+ * Renames an existing label with the specified revised name for all repos in an org.
+ */
 function* rename(args, program) {
   var org       = args[0]
   var label     = args[1]
@@ -68,6 +95,11 @@ function* rename(args, program) {
   return yield* handle_label(org, 'PATCH', { name: new_label, ext: label }, 'done renaming labels')
 }
 
+/*
+ * Standardizes a json list of labels across all repos in an org.
+ *
+ * The json list must reside in a repo at config/github_labels.json
+ */
 function* standardize(args, program) {
   var org         = args[0]
   var config_repo = args[1]
@@ -84,6 +116,7 @@ function* standardize(args, program) {
 
   console.log('GitHub rate limit remaining: ' + res.headers['x-ratelimit-remaining'])
 
+  // github sends the body (json file) as base64
   var config = JSON.parse(new Buffer(res.body.content, 'base64').toString('utf8'))
   if (!Array.isArray(config))
     throw new Error('error: github_labels.json must be a json array')
@@ -95,7 +128,6 @@ function* standardize(args, program) {
   while (i--) {
     reqs.push(handle_repo_labels(org, repos[i], config))
   }
-
   var results = yield reqs
 
   var total = log_results(results)
@@ -104,6 +136,11 @@ function* standardize(args, program) {
   console.log('done standardizing labels')
 }
 
+/*
+ * Handles differences between existing labels and a config list of labels.
+ *
+ * returns an array of responses
+ */
 function* handle_repo_labels(org, repo, config) {
 
   var url = 'https://api.github.com/repos/' + org + '/' + repo + '/labels'
@@ -136,6 +173,12 @@ function* handle_repo_labels(org, repo, config) {
   return yield results
 }
 
+/*
+ * Compares a list of wanted labels to a list of existing labels
+ *  and determines the differences.existing
+ *
+ * returns a list of objects containing the needed JSON body and http method.
+ */
 function compare_labels(config, existing) {
   var out = []
   var i   = config.length
@@ -168,6 +211,11 @@ function compare_labels(config, existing) {
   return out
 }
 
+/*
+ * Gets information about all of a GitHub user's (public?) repos.
+ *
+ * returns a list of repos
+ */
 function* get_repos(org) {
   var res = yield request({
       url:     'https://api.github.com/orgs/' + org + '/repos'
@@ -188,6 +236,13 @@ function* get_repos(org) {
   return repos
 }
 
+/*
+ * Handles getting repos and sending requests for single-label commands.
+ *
+ * See `send_label` for options
+ *
+ * returns an array of responses
+ */
 function* handle_label(org, method, opts, done) {
   var repos   = yield* get_repos(org)
   var results = yield* send_label(org, repos, opts, method)
@@ -202,6 +257,16 @@ function* handle_label(org, method, opts, done) {
   return yield results
 }
 
+/*
+ * Applies a label via method & options to all repos.
+ *
+ * Options can contain:
+ *   - The outgoing json, sent as the entire options.
+ *   - The url extension.
+ *   - The http method, if not otherwise specified.
+ *
+ * returns an array of responses
+ */
 function* send_label(org, repos, opts, method) {
   var arr = []
   var i   = repos.length
@@ -220,6 +285,11 @@ function* send_label(org, repos, opts, method) {
   return yield arr
 }
 
+/*
+ * Logs a two-dimensional [][] array of results.results
+ *
+ * returns the total number of results
+ */
 function log_results(results) {
   var total = 0
   var i     = results.length
@@ -237,6 +307,9 @@ function log_results(results) {
   return total
 }
 
+/*
+ * Logs a single response object.
+ */
 function log_result(result, label) {
   label = label || result.body.name
 
